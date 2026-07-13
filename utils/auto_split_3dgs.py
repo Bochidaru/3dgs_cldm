83932e262a32498e45d4edede78ebf10ee27f59b
Trương Minh Phương <truongminhphuong4@gmail.com>
2026-07-13 23:33:00 +0700

    small fix 2.1.3

--- utils/auto_split_3dgs.py
+++ utils/auto_split_3dgs.py
@@ -420,7 +420,7 @@ def prepare_auto_split(
         split_init_policy = init_policy
 
     valid_train_views = {"full", "3", "4", "6", "9", "12", "15", "18", "21", "24", "27", 
-                         "30", "33", "36", "39", "42", "45"}
+                         "30", "33", "36", "39", "42", "45", "48", "51", "54", "57", "60"}
     valid_copy_modes = {"copy", "hardlink", "symlink"}
     valid_sample_modes = {"paper_even", "pose_fps", "cluster_stride"}
     valid_init_policies = {"sparsegs_triangulate", "subset_colmap", "train_only_mapper", "train_only_colmap"}
